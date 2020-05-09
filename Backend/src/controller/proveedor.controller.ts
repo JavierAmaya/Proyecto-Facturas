97a29b8cd98ca9f37a23da7ed0d6b5d2152fb2ab
JavierAmaya <photojavhn@gmail.com>
2020-05-09 14:41:57 -0600

    otros cambios

--- Backend/src/controller/proveedor.controller.ts
+++ Backend/src/controller/proveedor.controller.ts
@@ -1,19 +1,26 @@
 import {Application} from "express";
-import {ProveedorService} from "../services/Proveedor.service";
+import {ProveedorService} from "../services/proveedor.service";
 
 export class  ProveedorController{
-    Proveedor_Service:ProveedorService;
+    
+    proveedor_pervice:ProveedorService;
+   
     constructor(private app: Application) {
-        this.Proveedor_Service = new ProveedorService();
+        this.proveedor_pervice = new ProveedorService();
         this.routes();
     }
 
     private routes(){
-        this.app.route("/Proveedor").post(this.Proveedor_Service.createOne);
-        this.app.route("/Proveedores").get(this.Proveedor_Service.getAll);
+        
+        this.app.route("/Proveedor").
+        post(this.proveedor_pervice.createOneProveedor);
+        
+        this.app.route("/Proveedores")
+        .get(this.proveedor_pervice.getAllProveedores);
+        
         this.app.route("/Proveedor/:id")
-        .get(this.Proveedor_Service.getOne)
-        .put(this.Proveedor_Service.updateOne)
-        .delete(this.Proveedor_Service.deleteOne);
+        .get(this.proveedor_pervice.getOneProveedor)
+        .put(this.proveedor_pervice.updateOneProveedor)
+        .delete(this.proveedor_pervice.deleteOneProveedor);
     }
 }
