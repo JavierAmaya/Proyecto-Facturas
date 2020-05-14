30bdf7428179d985f8ca8e5ce9b907e7fb090d5c
Edwin Quintero <EdwinQuintero01246>
2020-05-14 17:04:30 -0600

    NAVBAR TERMINADA

--- Frontend/src/components/navbarO.tsx
+++ Frontend/src/components/navbarO.tsx
@@ -7,33 +7,26 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import logo from "../img/logo-blanco.png";
-
+import cerrar_seccion from "../img/cerrar_seccion.png";
+import cambiodeusuario from "../img/cambio-de-usuario2.png";
 
 const NavBarO: React.FC = () => (
     <Navbar className="navbar" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="Main">
               <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top"/>{' '}
             Ferreteria La Unión
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-            <Nav.Link href="#home">Inventario</Nav.Link>
-            <Nav.Link href="#link">Cliente</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <Nav.Link href="Inventario">Inventario</Nav.Link>
+            <Nav.Link href="Contabilidad">Contabilidad</Nav.Link>
+            <NavDropdown title="Más" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1"><img src={cambiodeusuario} alt=""/>{' '}Cambiar Usuario</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2"><img src={cerrar_seccion} alt=""/>{' '}Cerrar Sesión</NavDropdown.Item>
             </NavDropdown>
             </Nav>
-            <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-            </Form>
         </Navbar.Collapse>
    </Navbar>
 );
-
 export default NavBarO;
