46f9af4517774a6986abc030868a4d893c53304e
LPSoldier19 <rafael.bautista1@hotmail.es>
2020-05-14 16:31:15 -0600

    Primeros pasos para crear la cardInformacion

---
+++ Frontend/src/components/cardInformacion.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import proveedorImg from "../img/global.png";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const CardInformacion: React.FC = () => (
+    <h1>Hola</h1>
+);
+
+export default CardInformacion;
---
+++ Frontend/src/views/detalle.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import {} from "react-router-dom";
+import Header2,{} from "../components/header2";
