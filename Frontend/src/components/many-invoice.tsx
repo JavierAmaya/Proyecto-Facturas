f44373c8d790b4a04c716bf78e683f3c3b7b9a1e
Edwin Quintero <EdwinQuintero01246>
2020-05-16 00:54:09 -0600

    commits

--- Frontend/src/components/many-invoice.tsx
+++ Frontend/src/components/many-invoice.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import Table from 'react-bootstrap/Table';
 import "../css/main.css";
 interface ImoduloConta{
     numero:Number
@@ -17,11 +14,15 @@ interface ImoduloConta{
 const Many_invoice : React.FC<ImoduloConta> = ({numero,Cliente,Factura,FechaEmitida,FechaLimite,Estado,Monto})=>(
     <tr>
         <td>{numero}</td>
-        <td>{Cliente}</td>
-        <td>{Factura}</td>
+        <td><Button id="ButtonListInvoice" href="/Contabilidad/:id/Facturas/:id" variant="light" size="lg">{Cliente}</Button></td>
+        <td><Button id="ButtonListInvoice" href="/Contabilidad/:id/Facturas/:id" variant="light" size="lg">{Factura}</Button></td>
         <td>{FechaEmitida}</td>
         <td>{FechaLimite}</td>
-        <td>{Estado}</td>
+        <td>
+            <Form>
+                <Form.Check type="switch"  id={Estado} label={Estado}/>
+            </Form>
+        </td>
         <td>L. {Monto}</td>
     </tr>  
 );
--- Frontend/src/components/manyinvoice.tsx
+++ Frontend/src/components/manyinvoice.tsx
@@ -4,14 +4,21 @@ import Many_Invoice from './many-invoice';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import "../css/main.css";
 const ManyInvoice: React.FC = () =>(
-    <div className="col-8" id="ListInvoice">
-        <Form inline>
-            <FormControl type="text" placeholder="Search" className="col-6" />
-            <Button variant="outline-success">Search</Button>
+    <div className="col-9" id="ListInvoice">
+        <Form inline className="col-12" id="FormInvoiceSearch">
+            <FormControl type="text" placeholder="Search" id="SearchInvoice" className="col-6 "/>
+            <Button className="col-2" variant="light" id="ButtonSearch">Search</Button>
         </Form>
-        <Table  striped bordered hover>
+        <Breadcrumb  id="BreadcrumbListInvoice" >
+            <Breadcrumb.Item href="Contabilidad">Contabilidad</Breadcrumb.Item>
+            <Breadcrumb.Item active href="Contabilidad/Facturas">
+                Facturas
+            </Breadcrumb.Item>
+        </Breadcrumb>
+        <Table  striped bordered hover size="md" variant="light">
             <thead>
                 <tr>
                 <th>#</th>
@@ -25,6 +32,7 @@ const ManyInvoice: React.FC = () =>(
             </thead>
             <tbody>
                 <Many_Invoice numero={1} Cliente="Marisol Carranza" Factura={125} FechaEmitida={"10/05/2019"} FechaLimite={"10/06/2019"} Estado={"Pendiente"}Monto={12589}/>
+                <Many_Invoice numero={2} Cliente="Alonzo Rivera" Factura={127} FechaEmitida={"14/06/2019"} FechaLimite={"14/07/2019"} Estado={"Pagado"}Monto={12589}/>
             </tbody>
         </Table>
     </div>
